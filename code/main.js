634e079cccc5e7be54abdb984e518596c979806a
Joe Alves <joseph.michael.alves@gmail.com>
2021-10-23 20:42:41 -0400

    load levels

--- code/main.js
+++ code/main.js
@@ -13,11 +13,11 @@ import loadLevel4 from "./levels/level4";
 import loadLevel5 from "./levels/level5";
 import loadLevel6 from "./levels/level6";
 const LEVEL_CONFIG = [
-    // loadLevel1,
-    // loadLevel2,
-    // loadLevel3,
-    // loadLevel4,
-    // loadLevel5,
+    loadLevel1,
+    loadLevel2,
+    loadLevel3,
+    loadLevel4,
+    loadLevel5,
     loadLevel6,
 ];
 
